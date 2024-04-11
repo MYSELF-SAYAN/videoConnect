b422f7f887658281589d90d0bbb234d9e15e31a9
MYSELF-SAYAN <116438215+MYSELF-SAYAN@users.noreply.github.com>
2024-04-11 12:44:05 +0530

    commit

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: Request) {
 
   // CREATE User in mongodb
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } =
+    const { id, email_addresses,  username } =
       evt.data;
 
     const user = {
